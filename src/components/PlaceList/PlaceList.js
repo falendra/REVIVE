32d10fcf3b3ae89ca7cf9f92d9aa87330ef7ed7f
Falendra Bandhe <sahilbandhe@gmail.com>
2019-06-24 13:03:10 +0530

    flatlist view added

--- src/components/PlaceList/PlaceList.js
+++ src/components/PlaceList/PlaceList.js
@@ -1,23 +1,29 @@
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
+import { FlatList, StyleSheet } from 'react-native';
 
 import ListItem from '../ListItem/ListItem';
 
-const placeList = props => {
-    const placesOutput = props.places.map((place, i) => (
-        <ListItem key={i} 
-        placeName={place} 
-        onItemPressed={()=>props.onItemDeleted(i)}/>
-      ));
-    return (
-        <ScrollView style={styles.listContainer}>{placesOutput}</ScrollView>
-    );
-};
 
+
+const placeList = props => {
+  return (
+    <FlatList
+      style={styles.listContainer}
+      data={props.places}
+      renderItem={(info) => (
+        <ListItem
+          placeName={info.item.name}
+          placeImage={info.item.image}
+          onItemPressed={() => props.onItemDeleted(info.item.key)} />
+      )}
+
+    />
+  );
+}
 const styles = StyleSheet.create({
-    listContainer: {
-      width: "100%"
-    }
+  listContainer: {
+    width: "100%"
+  }
 });
 
 export default placeList;
