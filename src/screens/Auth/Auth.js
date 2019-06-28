e1c8052ad52d62f0527ae97e68d9ca1d9da80b9f
Falendra Bandhe <sahilbandhe@gmail.com>
2019-06-29 02:49:00 +0530

    styling view

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -1,33 +1,43 @@
-import React ,{Component} from "react";
-import {View,Text,Button,TextInput,StyleSheet} from "react-native"
+import React, { Component } from "react";
+import { View, Text, Button, TextInput, StyleSheet } from "react-native"
 import startMainTabs from '../MainTabs/startMainTabs'
 
-class AuthScreen extends Component{
+class AuthScreen extends Component {
 
-    loginHandler=()=>{
+    loginHandler = () => {
         startMainTabs();
     }
-    render(){
-        return(
+    render() {
+        return (
             <View style={styles.container}>
                 <Text>Please Login</Text>
-                <TextInput placeholder="Your E-mail Address"/>
-                <TextInput placeholder="Password"/>
-                <TextInput placeholder="Confirm Password"/>
-                <Button title ="Login" onPress={this.loginHandler}/>
+                <View style={styles.inputContainer}>
+                    <TextInput placeholder="Your E-mail Address" style={styles.input}/>
+                    <TextInput placeholder="Password" style={styles.input} />
+                    <TextInput placeholder="Confirm Password" style={styles.input} />
+                </View>
+
+                <Button title="Login" onPress={this.loginHandler} />
             </View>
         );
 
-        
+
     }
 }
 
 export default AuthScreen;
 
 
-styles= StyleSheet.create({
-    container:{
-        borderColor:"red",
-        borderWidth:2,
+styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    inputContainer: {
+        width: "80%"
+    },
+    input: {
+        width: "100%"
     }
 })
