4686971bf457e1760ae6d5b4477f2c005c57b7f9
Falendra Bandhe <sahilbandhe@gmail.com>
2019-06-30 01:50:35 +0530

    component for picking image

---
+++ src/components/PickImage/PickImage.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { View, Image, Button,StyleSheet } from "react-native";
+import imagePlaceholder from "../../assets/previewPlace.jpg";
+
+
+class PickImage extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <View style={styles.placeholder}>
+                    <Image source={imagePlaceholder} style={styles.previewImage} />
+                </View>
+
+                <View style={styles.button}>
+                    <Button title="pick Image" onPress={()=>alert("pick image")} />
+                </View>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        width:"100%",
+        alignItems: "center"
+    },
+    placeholder: {
+        borderWidth: 1,
+        backgroundColor: '#eee',
+        borderColor: "black",
+        width: "80%",
+        height: 150,
+
+    },
+    button: {
+        margin:8
+    },
+    previewImage:{
+        width:"100%",
+        height:"100%"
+    }
+})
+
+
+export default PickImage;
