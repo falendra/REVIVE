b30791f9ed9022d77190e56ac03e16a5c0c6c464
Falendra Bandhe <sahilbandhe@gmail.com>
2019-06-30 01:50:52 +0530

    component for picking location

---
+++ src/components/PickLocation/PickLocation.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { View, Text, Button, StyleSheet } from "react-native";
+
+class PickLocation extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <View style={styles.placeholder}>
+                    <Text> map</Text>
+                </View>
+
+                <View style={styles.button}>
+                    <Button title="Locate me" />
+                </View>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        width:"100%",
+        alignItems: "center"
+    },
+    placeholder: {
+        borderWidth: 1,
+        backgroundColor: '#eee',
+        borderColor: "black",
+        width: "80%",
+        height: 200,
+
+    },
+    button: {
+        margin: 8
+    },
+    
+})
+
+
+export default PickLocation;
