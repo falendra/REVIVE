73fbd4ed17556ebd36326041bf03e183e8e9f04a
Falendra Bandhe <sahilbandhe@gmail.com>
2019-07-05 13:31:08 +0530

    nav bar styling

--- src/screens/FindPlace/FindPlace.js
+++ src/screens/FindPlace/FindPlace.js
@@ -5,6 +5,12 @@ import {connect } from "react-redux";
 
 
 class FindPlaceScreen extends Component {
+
+    static navigatorStyle = {
+      
+        navBarButtonColor :"orange"
+      };
+
     constructor(props){
         super(props);
         this.props.navigator.setOnNavigatorEvent(this.onNavigatorEvent);
