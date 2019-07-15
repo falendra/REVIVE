3557d859a699f56b719d697eb22c1c2a7f8e9b77
Falendra Bandhe <sahilbandhe@gmail.com>
2019-07-16 00:14:02 +0530

    firebase database implemented

--- src/store/actions/places.js
+++ src/store/actions/places.js
@@ -1,10 +1,19 @@
 import {ADD_PLACE,DELETE_PLACE} from "./actionTypes"
 export const addPlace=(placeName,location,image)=>{
-    return {
-        type: ADD_PLACE,
-        placeName:placeName,
-        location:location,
-        image:image
+    return dispatch => {
+        const placeData = {
+            name: placeName,
+            location: location
+        };
+        fetch("https://myapk-react-native.firebaseio.com/places.json", {
+            method: "POST",
+            body: JSON.stringify(placeData)
+        })
+        .catch(err => console.log(err))
+        .then(res => res.json())
+        .then(parsedRes => {
+            console.log(parsedRes);
+        });
     };
 
 };
