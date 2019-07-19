5d052a153642e5a79137d1814931130954f0025e
Falendra Bandhe <sahilbandhe@gmail.com>
2019-07-19 05:52:34 +0530

    refreshing token without app reload

--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -2,7 +2,7 @@ import {AUTH_SET_TOKEN,AUTH_REMOVE_TOKEN} from "../actions/actionTypes";
 
 const initialState={
     token:null,
-    
+    expiryDate:null
 };
 const reducer=(state=initialState,action)=>{
 
@@ -11,14 +11,14 @@ const reducer=(state=initialState,action)=>{
             return{
                 ...state,
                 token:action.token,
-               
+                expiryDate:action.expiryDate
             };
 
         case AUTH_REMOVE_TOKEN :
             return{
                 ...state,
                 token:null,
-                
+                expiryDate:null
             }   
         default:
             return state;
